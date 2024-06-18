b4af82fe8bee2f1e23322ed6a72ba42500bf5bf3
Mahesh Rohane <shravanrohane19@gmail.com>
2024-06-18 16:28:14 +0530

    url changed

--- client/src/services/Auth.js
+++ client/src/services/Auth.js
@@ -1,11 +1,11 @@
 import axios from 'axios';
 
-// const URL = 'https://assignment-ed6x.onrender.com';
-const LOCALURL = 'http://localhost:8080';
+const URL = 'https://assignment-ed6x.onrender.com';
+// const LOCALURL = 'http://localhost:8080';
 
 export const login = async(data) => {
     try{
-       return await axios.post(`${LOCALURL}/login`,data);
+       return await axios.post(`${URL}/login`,data);
     }
     catch(err){
         console.log("Error occurs while running login User function",err);
