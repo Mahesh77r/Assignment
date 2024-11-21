47086f009c6e58eeccaa348190430904787439e8
Mahesh77r <shravanrohane19@gmail.com>
2024-11-22 02:30:22 +0530

    new apis

--- server/router/routes.js
+++ server/router/routes.js
@@ -1,7 +1,8 @@
 const router = require('express').Router();
 const {loginUser, resetPassword, generateToken} = require('../controller/AUTH')
 const {createUser,deleteUser,getUser,updateUser} = require('../controller/CRUD')
-const auth = require("../middelwares/Auth");
+const {addCases, getCases} = require('../controller/Cases')
+const auth = require("../middlewares/Auth");
 
 // Auth
 router.post('/login',loginUser);
@@ -13,5 +14,10 @@ router.post('/user',createUser);
 router.get('/user/:id',auth,getUser);
 router.patch('/user/:id',auth,updateUser);
 router.delete('/user/:id',auth,deleteUser);
+
+
+router.post('/cases',addCases);
+router.get('/cases/',getCases);
+router.get('/cases/:id',getCases);
 
 module.exports = router
